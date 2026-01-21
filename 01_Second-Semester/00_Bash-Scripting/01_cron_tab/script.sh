b053f77620f382847762d5cbd37f89b77f030b24
Mareola-Mabs <bulun1001@gmail.com>
2026-01-21 16:08:15 +0100

    feat: add crontab

---
+++ 01_Second-Semester/00_Bash-Scripting/01_cron_tab/script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+PATHTOFILE=/home/mareola/Desktop/Tech/Software_Engineering/Cloud_Engineering/ALT_School_Africa/ALT_School_Repo/01_Second-Semester/00_Bash-Scripting/01_cron_tab/commands.log
+
+pwd
+
+free -h >> "$PATHTOFILE"
